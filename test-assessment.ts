a34beea1353597c7d5a2936947515d98ce83c1f9
Michel Tsuboi <micheltsuboi@MacBook-Air-de-Michel.local>
2026-03-06 13:06:14 -0300

    chore: push automático

---
+++ test-assessment.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env.local' })
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+// But we need auth to test RLS!
+console.log('Use view_file to instead check where the code fails')
